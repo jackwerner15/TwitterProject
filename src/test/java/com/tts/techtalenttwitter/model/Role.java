8b2b04a8501c698e8cf22996ceeb2eb4d8c7ea83
Jack Werner <jwerner@Jacks-MacBook-Pro-2.local>
2020-10-27 09:48:44 -0400

    completed role model

---
+++ src/test/java/com/tts/techtalenttwitter/model/Role.java
@@ -0,0 +1,52 @@
+package com.tts.techtalenttwitter.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private Long id;
+    private String roles;
+
+    public Role(){}
+
+    public Role(Long id, String roles) {
+        this.id = id;
+        this.roles = roles;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public String toString() {
+        return "Role [id=" + id + ", roles=" + roles + "]";
+    }
+
+    
+    
+    
+
+    
+}
